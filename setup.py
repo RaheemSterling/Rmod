829e85055a34bcaac0cae11ee93fdb02814cd7aa
RaheemSterling <45504472+RaheemSterling@users.noreply.github.com>
2026-01-03 23:21:54 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rmod',
+    version='0.1.0',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    author='',
+    description='Minimal structural revision for S5 epistemic models',
+    license='MIT',
+)
